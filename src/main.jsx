4a3bb37e4ef71825af865ccb8ce8ad04096cd179
dev-am4 <am4.production29@gmail.com>
2026-09-10 16:58:26 +0700

    Cinematic fullscreen person detail V4
    Use a large fullscreen portrait with the story, DNA and career content overlaid beside it on the same touchscreen view.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ExhibitV3 from './ExhibitV3.jsx'
+import ExhibitV4 from './ExhibitV4.jsx'
 import './exhibit-v3.css'
+import './exhibit-v4.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ExhibitV3 />
+    <ExhibitV4 />
   </React.StrictMode>,
 )
