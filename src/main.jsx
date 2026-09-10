98af30255ca37d90d3f49da46d87ebb22c86ccf8
dev-am4 <am4.production29@gmail.com>
2026-09-10 17:23:42 +0700

    Space Touch V7
    Restore space visual language, use bundled Chakra Petch square typography, and replace hover-dependent interactions with touch press feedback.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource/chakra-petch/400.css'
+import '@fontsource/chakra-petch/500.css'
+import '@fontsource/chakra-petch/600.css'
+import '@fontsource/chakra-petch/700.css'
 import ExhibitV6 from './ExhibitV6.jsx'
 import './exhibit-v6.css'
+import './exhibit-v7.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
