4811a866885db92cc34a2553cb613e4f47af09fa
dev-am4 <am4.production29@gmail.com>
2026-09-10 17:07:00 +0700

    Apply editorial V5 detail overrides

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import ExhibitV4 from './ExhibitV4.jsx'
 import './exhibit-v3.css'
 import './exhibit-v4.css'
+import './exhibit-v5.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
