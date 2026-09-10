ae179e2ca594d03f64abec8d6e1af2b73b96e41c
dev-am4 <am4.production29@gmail.com>
2026-09-10 16:11:41 +0700

    Switch entrypoint to immersive touchscreen V2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './styles.css'
+import ImmersiveV2 from './ImmersiveV2.jsx'
+import './immersive-v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ImmersiveV2 />
   </React.StrictMode>,
 )
