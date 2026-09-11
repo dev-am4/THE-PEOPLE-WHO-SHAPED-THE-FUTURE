c9fc493a9f757ec0ac897637e83dd43625b395f1
dev-am4 <am4.production29@gmail.com>
2026-09-11 10:48:15 +0700

    Use story-first V16 detail presentation

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import '@fontsource/chakra-petch/400.css'
 import '@fontsource/chakra-petch/500.css'
 import '@fontsource/chakra-petch/600.css'
 import '@fontsource/chakra-petch/700.css'
-import ExhibitV14 from './ExhibitV14.jsx'
+import ExhibitV15 from './ExhibitV15.jsx'
 import './exhibit-v14.css'
 import './gallery-portrait-grid.css'
 import './detail-open-layout.css'
+import './detail-story-first.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ExhibitV14 />
+    <ExhibitV15 />
   </React.StrictMode>,
 )
