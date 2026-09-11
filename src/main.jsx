6752aafd166dce104985a1b700d7757ff90cef43
dev-am4 <am4.production29@gmail.com>
2026-09-11 09:03:46 +0700

    Load V11 kiosk refinement overrides

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import '@fontsource/chakra-petch/600.css'
 import '@fontsource/chakra-petch/700.css'
 import ExhibitV11 from './ExhibitV11.jsx'
 import './exhibit-v11.css'
+import './exhibit-v11-fixes.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
