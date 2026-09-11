4e386b4689c324884e30546d2141288fe0c69f40
dev-am4 <am4.production29@gmail.com>
2026-09-11 10:39:00 +0700

    Load open profile detail layout

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import '@fontsource/chakra-petch/700.css'
 import ExhibitV14 from './ExhibitV14.jsx'
 import './exhibit-v14.css'
 import './gallery-portrait-grid.css'
+import './detail-open-layout.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
