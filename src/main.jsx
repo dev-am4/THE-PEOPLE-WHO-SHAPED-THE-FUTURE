27bf72cb37dac2e82caa4207bec15700c0882c5d
dev-am4 <am4.production29@gmail.com>
2026-09-11 10:29:43 +0700

    Load portrait-first archive grid styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import '@fontsource/chakra-petch/600.css'
 import '@fontsource/chakra-petch/700.css'
 import ExhibitV14 from './ExhibitV14.jsx'
 import './exhibit-v14.css'
+import './gallery-portrait-grid.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
