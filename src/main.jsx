6293196ebd0ff78ffada8223054ac0bbb8b1304d
dev-am4 <am4.production29@gmail.com>
2026-09-11 09:02:16 +0700

    Switch app entry to V11 experience

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,11 @@ import '@fontsource/chakra-petch/400.css'
 import '@fontsource/chakra-petch/500.css'
 import '@fontsource/chakra-petch/600.css'
 import '@fontsource/chakra-petch/700.css'
-import ExhibitV6 from './ExhibitV6.jsx'
-import './exhibit-v6.css'
-import './exhibit-v7.css'
-import './exhibit-v8.css'
-import './exhibit-v9.css'
-import './exhibit-v10.css'
+import ExhibitV11 from './ExhibitV11.jsx'
+import './exhibit-v11.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ExhibitV6 />
+    <ExhibitV11 />
   </React.StrictMode>,
 )
